c4e89bbcafc2490f31fadceb48ad0f048d0ed80b
MoriMoriMori <78454491+MoriMoriMorii@users.noreply.github.com>
2024-01-12 21:30:18 -0600

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,11 +1,11 @@
 <?php
 $suggestion = $_POST['suggestion'];
-$file_path = 'path/to/suggestions.txt';
+$file_path = './suggestions.txt';
 
 // Append the suggestion to the text document
 file_put_contents($file_path, $suggestion . PHP_EOL, FILE_APPEND);
 
 // Redirect back to the suggestion form or any other desired page
-header("Location: suggestion_form.html");
+header("Location: index.html");
 exit();
 ?>
